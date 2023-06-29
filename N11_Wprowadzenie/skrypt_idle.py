49dffb33ae7f42d32d1778e19f9e8b95445732f0
Adam Jurkiewicz Pythonista Dev <adam@jurkiewicz.tech>
2023-06-29 12:05:54 +0200

    N11 - kody do prezentacji

---
+++ N11_Wprowadzenie/skrypt_idle.py
@@ -0,0 +1,15 @@
+imie = "Adam "
+nazwisko = "Jurkiewicz"
+rok_urodzenia = 1974
+logiczna_prawda = True
+
+wiek = 2022 - rok_urodzenia
+imie_nazwisko = imie + nazwisko
+
+# poniższe nie wyświetli nic w trybie skryptowym
+wiek
+imie_nazwisko
+
+# poniższe wyświetli zawartość zmiennych w trybie skryptowym
+print(wiek)
+print(imie_nazwisko)
